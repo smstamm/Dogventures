3cbbd14c32c2e1f54e34c8b78a1e1221a3130529
Steph Stamm <steph.m.stamm@gmail.com>
2019-01-21 17:01:13 -0700

    adds redux for async actions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './rootReducer';
 import AppRoot from './AppRoot';
 import * as serviceWorker from './serviceWorker';
+import promiseMiddleware from 'redux-promise-middleware';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-ReactDOM.render(<AppRoot />, document.getElementById('root'));
+import './index.css';
+
+const store = process.env.NODE_ENV === 'development' ?
+  createStore(rootReducer, composeWithDevTools(applyMiddleware(promiseMiddleware()))) :
+  createStore(rootReducer, applyMiddleware(promiseMiddleware()));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRoot />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import PetsReducer from './reducers';
+
+export default combineReducers({
+  PetsReducer
+});
